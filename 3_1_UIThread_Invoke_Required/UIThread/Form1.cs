fb40f0b8ba7150ed9cc94f5be22c05c179fce74f
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-07-24 20:44:02 +0300

    Add files via upload

---
+++ 3_1_UIThread_Invoke_Required/UIThread/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateLabel()
+        {
+            if (label1.InvokeRequired)
+            {
+                Action a = () => { label1.Text = DateTime.Now.Millisecond.ToString(); };
+                label1.BeginInvoke(a);
+            }
+            else
+            {
+                label1.Text = DateTime.Now.Millisecond.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                //label1.Text = DateTime.Now.Millisecond.ToString();
+                UpdateLabel();
+            });
+        }
+    }
+}
